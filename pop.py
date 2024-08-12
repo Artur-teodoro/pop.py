d5c55cc4e268a66e9970728b67c06b59e33cad5f
Artur <arturfreitas0909@gmail.com>
2024-08-12 15:52:04 -0300

    phyton

---
+++ pop.py
@@ -0,0 +1,11 @@
+class Caneca:
+    cor = "amarelo"
+    marca = "Stanley"
+    formato = "cilindrico"
+
+caneca1 = Caneca()
+print(caneca1.cor)
+
+caneca2 = Caneca()
+caneca2.cor="azul"
+print(caneca2.cor) 
---
+++ welcome.py
@@ -0,0 +1,27 @@
+# nome = input("qual é seu nome :")
+# idade = input("qual é a sua idade :")
+
+# if int(idade) >= 18:
+#     print(" voce é maior de idade.")
+# else:
+#     print(" voce é menor de idade.")
+
+# a = 5
+# b = 5
+
+# resultado = a + b 
+# print("adição", resultado)
+# resultado = a * b 
+# print("multiplicação", resultado)
+# resultado = a / b
+# print("divisão", resultado)
+
+# v = True
+
+frutas = ["maçã", "banana", "cereja"]
+for fruta in frutas:
+    print(fruta)
+
+for i in range(5):  # Isso vai iterar de 0 a 4
+    print(i)
+
